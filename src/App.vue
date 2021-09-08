b695b005fd27ce4325e76b4639b47d44f819d92c
Matheus Dias <dias.math0@gmail.com>
2021-09-08 00:01:44 -0300

    Criação da Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <v-app>
-    <v-main>
+    <MylevNavbar />
+
+    <v-main class="backgroudColor">
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import MylevNavbar from "./components/Mylev-Navbar.vue";
+
 export default {
   name: "App",
+
+  components: {
+    MylevNavbar,
+  },
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Changa+One&display=swap');
+
+.heightNavFooter {
+  height: 80px !important;
+}
+
+.color {
+  background-color: #499fc6 !important;
+}
+
+.v-application {
+  font-family: 'Changa One';
+}
+
+.textColor {
+  color: #333 !important;
+}
+
+.backgroudColor {
+  background-color: #899397;
+}
+</style>
