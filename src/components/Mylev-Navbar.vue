b607ba616528bcc8ba0fb2f594cc1e54cdd77826
Matheus Dias <dias.math0@gmail.com>
2021-09-11 20:06:12 -0300

    Criação do Card de Matérias

--- src/components/Mylev-Navbar.vue
+++ src/components/Mylev-Navbar.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <v-app-bar class="color heightNavFooter" flat dense>
-      <v-toolbar-title><v-img max-height="60" max-width="60" :src="require('../assets/logo.png')"></v-img></v-toolbar-title>
+      <v-toolbar-title
+        ><v-img
+          max-height="60"
+          max-width="60"
+          :src="require('../assets/logo.png')"
+        ></v-img
+      ></v-toolbar-title>
 
       <v-spacer></v-spacer>
 
@@ -12,31 +18,37 @@
       </v-row>
 
       <v-row class="hidden-md-and-up">
-          <v-menu offset-y>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn icon class="float-right color" v-bind="attrs" v-on="on"
-                ><v-icon>mdi-menu</v-icon></v-btn
-              >
-            </template>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon class="float-right color" v-bind="attrs" v-on="on"
+              ><v-icon>mdi-menu</v-icon></v-btn
+            >
+          </template>
 
-            <v-list class="color">
-              <v-list-item>
-                <v-btn elevation="0" class="textColor" text>Home</v-btn>
-              </v-list-item>
+          <v-list class="color">
+            <v-list-item>
+              <v-btn elevation="0" class="textColor" text>Home</v-btn>
+            </v-list-item>
 
-              <v-list-item>
-                <v-btn elevation="0" class="textColor" text>Área do Aluno</v-btn>
-              </v-list-item>
+            <v-list-item>
+              <v-btn elevation="0" class="textColor" text>Área do Aluno</v-btn>
+            </v-list-item>
 
-              <v-list-item>
-                <v-btn elevation="0" class="textColor" text>Login</v-btn>
-              </v-list-item>
-            </v-list>
-          </v-menu>
+            <v-list-item>
+              <v-btn elevation="0" class="textColor" text>Login</v-btn>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-row>
     </v-app-bar>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Mylev-NavBar",
+};
+</script>
 
 <style>
 .v-toolbar__content {
@@ -44,7 +56,7 @@
 }
 
 .row {
-    margin: 0 !important;
-    justify-content: end;
+  margin: 0 !important;
+  justify-content: end;
 }
 </style>
