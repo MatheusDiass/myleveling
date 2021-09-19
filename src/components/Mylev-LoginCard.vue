0d575a54f41a1da6553cf9081200b35db36f2b94
Kaique3224 <kaio.x.arrocha@gmail.com>
2021-09-19 18:00:17 -0300

    criação do card de login

---
+++ src/components/Mylev-LoginCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="externalDivBorder">
+    <v-card class="paddingCard" color="#499fc6" flat>
+      <v-form>
+        
+        <label>Email:</label>
+        <v-text-field background-color="white" outlined></v-text-field>
+        <label>Senha</label>
+        <v-text-field background-color="white" outlined></v-text-field>
+
+        <div class="contentCenter">
+          <v-btn color="#3898ec">login</v-btn>
+        </div>
+
+        <div>
+          <br />
+          <v-row class="contentCenter"><label>Outras Opções</label></v-row>
+          <v-row class="contentCenter"><v-img max-height="60" max-width="60" :src="require('../assets/logoGoogle.png')"></v-img>
+          </v-row>
+        </div>
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
