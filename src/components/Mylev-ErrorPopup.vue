82817834f142a1cd1a4b64a1a576cf4d9cff8d26
Luis <luisft128@gmail.com>
2021-09-22 20:55:19 -0300

    Criação do Popup

---
+++ src/components/Mylev-ErrorPopup.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="externalDivBorder divSize" >
+    <v-alert class="teste contentCenter" color="#499fc6" width="400px" >
+      Mensagem de Erro!
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.teste{
+  margin-bottom: 0px;
+  display: flex;
+  justify-content: center;
+}
+.divSize{
+  width: 408px;
+}
+</style>
