92fedfe6c42b36553d0c02aca812f4ef09d60acf
Luis <luisft128@gmail.com>
2021-09-22 21:01:58 -0300

    Criação do SubjectCard

---
+++ src/components/Mylev-SubjectCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="externalDivBorder">
+    <v-card class="paddingCard" color="#499fc6" flat>
+      <v-form>
+        <label>Nomes</label>
+        <v-text-field background-color="white" outlined></v-text-field>
+
+        <div class="contentCenter">
+          <v-btn color="#3898ec">Salvar</v-btn>
+        </div>
+
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
