fbf1dc6df0ec08913cb01b0190aae878cbc0d2dc
Leonardo lima <leeofr_lima@hotmail.com>
2021-09-26 18:26:58 -0300

    Criação da pagina de editar matéria

---
+++ src/views/Mylev-EditSubject.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <h1>Editar matéria</h1>
+
+    <br />
+
+    <v-divider></v-divider>
+
+    <br />
+
+  
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'MylevEditSubject',
+
+
+}
+</script>
