7328ef52ae39e8cdfc675a066930b3b14a4e9420
Matheus Dias <dias.math0@gmail.com>
2021-10-03 03:00:50 -0300

    Criação da ação para deletar uma matéria

--- src/views/Mylev-ListSubject.vue
+++ src/views/Mylev-ListSubject.vue
@@ -8,18 +8,22 @@
 
     <br />
 
+    <MylevAlert />
+    
     <MylevTableSubjectCard />
   </v-container>
 </template>
 
 <script>
 import MylevTableSubjectCard from '../components/views/subject/Mylev-TableSubjectCard.vue'
+import MylevAlert from '../components/shared/Mylev-Alert.vue'
 
 export default {
     name: 'MylevListSubject',
 
     components: {
         MylevTableSubjectCard,
+        MylevAlert
     },
 }
 </script>
