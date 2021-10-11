11fefe61da7c9f6308cba3f6a1913961ebf80b9b
Matheus Dias <dias.math0@gmail.com>
2021-10-11 18:02:05 -0300

    Criado o card com o formulário para a confirmação do cadastro

---
+++ src/components/views/confirmregister/Mylev-ConfirmRegisterCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card class="paddingCard" color="#499fc6" rounded="lg" flat>
+        <v-form>
+            <label>Código:</label>
+            <v-text-field background-color="white" outlined></v-text-field>
+
+            <div class="contentCenter">
+                <v-btn color="#3898ec">Confirmar</v-btn>
+            </div>
+
+            <br>
+
+            <div class="contentCenter">
+                <a href="">Gerar um novo código</a>
+            </div>
+        </v-form>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'MylevConfirmRegisterCard',
+}
+</script>
+
+<style scoped>
+a {
+    color: black !important;
+}
+</style>
--- src/views/Mylev-ConfirmRegister.vue
+++ src/views/Mylev-ConfirmRegister.vue
@@ -1,17 +1,25 @@
 <template>
   <v-container>
-    <h1>Confirmar Email</h1>
+    <h1>Confirmar Cadastro</h1>
 
     <br />
 
     <v-divider></v-divider>
 
     <br />
+
+    <MylevConfirmRegister />
   </v-container>
 </template>
 
 <script>
+import MylevConfirmRegister from '../components/views/confirmregister/Mylev-ConfirmRegisterCard.vue';
+
 export default {
   name: 'MylevEditSubject',
+
+  components: {
+    MylevConfirmRegister,
+  }
 }
 </script>
