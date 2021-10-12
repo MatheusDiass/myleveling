c347b529062cf9c5d297c5a851d9e525e10f3e6e
Luis <luisft128@gmail.com>
2021-10-11 21:47:33 -0300

    Criação do popup de deleção

---
+++ src/components/shared/Mylev-Dialog.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-dialog :value="true" width="550">
+            <v-card class="paddingCard" color="#499fc6" rounded="lg" flat>
+                <v-form class="paddingCard">
+                <h2 class="font-weight-bold">Aviso!</h2><br>
+                <h3 class="font-weight-bold">Deseja realmente deletar?</h3><br>
+                <v-btn class="mr-10" color="#3898ec">Deletar</v-btn>
+                <v-btn color="#3898ec">Cancelar</v-btn>
+                </v-form>
+            </v-card>
+    </v-dialog>
+</template>
