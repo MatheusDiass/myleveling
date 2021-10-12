1e897602deef883db38e7cc60c349469d8c4f8a2
Luis <luisft128@gmail.com>
2021-10-11 21:06:05 -0300

    Criação do avatar do usuário

---
+++ src/components/shared/Mylev-UserImage.vue
@@ -0,0 +1,37 @@
+<template>
+<v-avatar :size="avatarSize" color="#499fc6">
+<span></span>
+</v-avatar>
+</template>
+
+<script>
+export default {
+    name: "MylevUserImage",
+    data(){
+        return{
+
+        }
+    },
+    props: {
+        isSmall: {
+            type: Boolean,
+            default: false
+        },
+        isLarge: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        avatarSize(){
+            if (this.isSmall){
+                return 28
+            } else if (this.isLarge){
+                return 60
+            } else {
+                return 38
+            }
+        }
+    }
+}
+</script>
