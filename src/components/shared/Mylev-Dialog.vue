f58662cc99e5d52f22dc6721892acefeb7f94534
Matheus Dias <dias.math0@gmail.com>
2021-10-21 01:53:13 -0300

    Adicionada algumas melhorias no componente Mylev-Dialog e utilizado o mesmo como confirmação para deletar uma matéria

--- src/components/shared/Mylev-Dialog.vue
+++ src/components/shared/Mylev-Dialog.vue
@@ -1,12 +1,74 @@
 <template>
-    <v-dialog :value="true" width="550">
+    <v-dialog  v-model="show" :fullscreen="$vuetify.breakpoint.mobile" width="550" persistent>
             <v-card class="paddingCard" color="#499fc6" rounded="lg" flat>
-                <v-form class="paddingCard">
-                <h2 class="font-weight-bold">Aviso!</h2><br>
-                <h3 class="font-weight-bold">Deseja realmente deletar?</h3><br>
-                <v-btn class="mr-10" color="#3898ec">Deletar</v-btn>
-                <v-btn color="#3898ec">Cancelar</v-btn>
-                </v-form>
+                <div class="pa-5">
+                    <h1>{{ title }}</h1>
+
+                    <br>
+
+                    <h3>{{ content }}</h3>
+
+                    <br>
+
+                    <v-btn v-if="confirmButton" color="#3898ec" class="mr-10" @click="confirm">{{ confirmButtonText }}</v-btn>
+                    <v-btn v-if="cancelButton" color="#3898ec" @click="closeDialog">{{ cancelButtonText }}</v-btn>
+                </div>
             </v-card>
     </v-dialog>
 </template>
+
+<script>
+export default {
+    name: 'Mylev-Dialog',
+
+    props: {
+        show: {
+            type: Boolean,
+            required: true
+        },
+
+        title: {
+            type: String,
+            required: true,
+        },
+
+        content: {
+            type: String,
+            required: true,
+        },
+
+        confirmButton: {
+            type: Boolean,
+            required: true,
+        },
+
+        confirmButtonText: {
+            type: String,
+            default: 'Ok',
+        },
+
+        cancelButton: {
+            type: Boolean,
+            required: true,
+        },
+
+        cancelButtonText: {
+            type: String,
+            default: 'Cancelar',
+        }
+    },
+
+    methods: {
+        //Emite um event para o componente pai fechar o dialogo
+        closeDialog() {
+            this.$emit('closeDialog', false);
+        },
+
+        /*Emite um event para o componente pai informando que
+        o botão de confirmação foi apertado*/
+        confirm() {
+            this.$emit('confirm');
+        }
+    }
+}
+</script>
